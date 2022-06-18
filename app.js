fe29499a5ee828c670ea6cc04c8c2dae7f118203
lopasov89 <lopasov89@gmail.com>
2022-06-18 19:06:22 +0400

    final 7.0

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 // ! Импортируем библиотеки
 require('dotenv').config() // подключили dotenv
 const express = require('express') // подключили экспресс
-// const createError = require('http-errors') // подключили обработку ошибок
 const path = require('path') // подключили модуль path
 const hbs = require('hbs') // подключили hbs
 const morgan = require('morgan') // подключили логгер morgan
-const dbCheck = require('./db/dbCheck'); // подключение скрипта проверки соединения с БД
 const session = require('express-session'); // подключили модуль для работы с сессиями
 const FileStore = require('session-file-store')(session); // подключили модуль для хранения сессий
+const dbCheck = require('./db/dbCheck'); // подключение скрипта проверки соединения с БД
 
 // ! Импортируем созданные в отдельный файлах роуты.
 // -->Тут пишем require роутов<--
 
 // ! Инициализируем приложение
 const app = express() // создали экземпляр сервера
-const PORT = process.env.PORT || 3000 // создали константу с портом
+const PORT = process.env.PORT || 3002 // создали константу с портом
 dbCheck(); // вызов функции проверки соединения с базоый данных
 
 // ! Задаем конфигурацию для работы с сессиями
@@ -40,16 +39,30 @@ app.use(express.urlencoded({ extended: true })) // Подключаем middlewa
 app.use(express.json()) // Подключаем middleware, которое позволяет читать переменные JavaScript, сохранённые в формате JSON в body HTTP-запроса.
 app.use(session(sessionConfig)) // Подключаем middleware для использования сессий в приложений и передаем ей конфигурацию 
 
-// ! -->Тут пишем роуты<--
+// ! мидлварка для того чтобы присвоить локальной переменной имя и id юзера из сессии
+app.use((req, res, next) => {
+  if (req.session.userId) {
+    res.locals.userName = req.session.userName
+    res.locals.userId = req.session.userId
+  }
+  next()
+})
+
+// ! Подключаем обработку запросов
+// -->Тут пишем роуты<--
 
 // ! Обработка ненайденных страниц
-// Если HTTP-запрос дошёл до этой строчки, значит ни один из ранее встречаемых рутов не
-// ответил на запрос. Это значит, что искомого раздела просто нет на сайте. Создаём небольшое middleware, которое отображает страницу с ошибкой
+// Если HTTP-запрос дошёл до этой строчки, значит ни один из ранее встречаемых рутов не ответил на запрос. 
+// Это значит, что искомого раздела просто нет на сайте.
+// Создаём небольшое middleware, которое отображает страницу с ошибкой
 app.get('*', (req, res) => {
-  res.render('error')
+  res.render('error', {
+    message: 'Такой страницы не существует',
+    error: {},
+  })
 })
 
 // ! Начинаем слушать порт для запуска сервера
-app.listen(PORT, () => { // начинаем слушать сервер на указанном порте
+app.listen(PORT, () => { 
   console.log(`Сервер запущен на порте ${PORT}`)
 })
---
+++ middlewares/check.middleware.js
@@ -0,0 +1,20 @@
+// проверка на то есть ли у юзера сессия
+const checkIsSession = (req, res, next) => {
+  if (req.session.userId) {
+    res.redirect('/')
+  } else {
+    next()
+  }
+}
+// проверка на то нет ли у юзера сессия
+const checkIsNotSession = (req, res, next) => {
+  if (!req.session.userId) {
+    res.redirect('/user/login')
+  } else {
+    next()
+  }
+}
+
+module.exports = {
+  checkIsSession, checkIsNotSession,
+}
